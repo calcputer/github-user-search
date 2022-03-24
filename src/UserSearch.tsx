3bab13449ad3fa63f219aed64fa6930c0204673d
Patrick <shipat13@gmail.com>
2022-03-23 20:31:30 -0500

    initial search api with users in list

--- src/UserSearch.tsx
+++ src/UserSearch.tsx
@@ -1,11 +1,31 @@
-import React, {FunctionComponent, useState } from "react";
+import React, {FunctionComponent, useEffect, useState } from "react";
+
+const BASE_URL = "https://api.github.com/search/users?q=";
+
 const UserSearch:FunctionComponent = () => {
     const [searchTerm, setSearchTerm] = useState<string>("");
+    const [query, setQuery] = useState<string>("");
+    const [results, setResults] = useState<[]>([]);
+
+    useEffect(() => {
+        if(query.length > 0){
+            fetch(BASE_URL + query)
+            .then((result) => result.json())
+            .then((data) => setResults(data.items)); 
+        } else {
+            setResults([]);
+        }
+    }, [query]);
+
+    const userCards = results?.map((user:any) => 
+        <li key={user.id}><a href={user.html_url}>{user.login}</a></li>
+    );
 
     return(
         <>
-            <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event?.target.value)} aria-label="Search Box"></input>
-            <input type="button" value="Search" onClick={() => console.log("searching for " + searchTerm)}></input>
+            <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event?.target.value)} aria-label="Search Box" autoFocus></input>
+            <input type="button" value="Search" onClick={() => setQuery(searchTerm)}></input>
+            <ul id="users-list">{userCards}</ul>
         </>
     );
 }
